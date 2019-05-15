ec8f5503c2e44ca356d54e239e35fa3c1a9d9313
Joe Jamsky <cba1067950@yahoo.com>
2019-05-15 18:09:41 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -18,16 +18,10 @@ class String
     count = 0
     array = self.split
     array.each do |elem|
-      #binding.pry
-
       if elem.end_with?(".") == true || elem.end_with?("?") == true || elem.end_with?("!") == true 
         count += 1
-        #binding.pry
       end 
-      #binding.pry
     end
-    #array = self.split("?")
-    #binding.pry 
     count 
   end
 end
