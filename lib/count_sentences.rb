29132d04e6d39097da6bf54d9cd06cc5e8effdac
Joe Jamsky <cba1067950@yahoo.com>
2019-05-15 18:28:58 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    array = self.split(/ \. | \? | \! /).delete_if {|w| w < 2 }
+    self.split(/ \. | \? | \! /).delete_if {|w| w.size < 2 }.size
     
     
     #array = self.split
